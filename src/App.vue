f2e80d9cea3ba225f1b96335e4a9c90cdc2ed4c0
macsienkiewicz <84861413+macsienkiewicz@users.noreply.github.com>
2022-06-19 17:49:41 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <body>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Movies :moviesSource="movies"/>
+  <router-view />
+  
+
 
 </body>
   
@@ -9,47 +11,6 @@
 </template>
 
 <script>
-import Movies from '@/components/Movies.vue'
-
-
-export default {
-    name: 'app',
-    components: {
-      Movies, 
-    },
-
-    data() {
-      return {
-        movies: []
-      }
-    },
-
-    mounted() {
-      this.getMovies()
-    },
-
-    methods: {
-      async getMovies() {
-        try {
-        //const response = await fetch('https://jsonplaceholder.typicode.com/users%27)
-        const response = await fetch("http://localhost:8080/movies")
-        const data = await response.json()
-        this.movies = data
-        } catch (error) {
-          console.error(error)
-          }      
-    },
-
-   
-  
-    
-
-    }
-
-    
-
-    
-  }
 </script>
 
 <style>
@@ -59,7 +20,98 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgb(255, 255, 255);
   margin-top: 60px;
 }
+
+
+.screen {
+  text-align: center;
+}
+
+
+
+.seat {
+    text-align: center;
+    background-color: #6aaf67;
+    color: rgb(255, 255, 255);
+    height: 40px;
+    width: 50px;
+    margin: 7px;
+    border-top-left-radius: 40px;
+    border-top-right-radius: 40px;
+}
+
+.seat.selected {
+    background-color: #6feaf6;
+}
+
+.seat.occupied {
+    background-color: rgb(91, 2, 2);
+}
+
+.seat:nth-of-type(4) {
+    margin-right: 40px;
+}
+
+.seat:nth-last-of-type(2) {
+    margin-left: 40px;
+}
+
+.seat:not(.occupied):hover {
+    cursor: pointer;
+    transform: scale(1.2);
+}
+
+
+.showcase .seat:not(.occupied):hover {
+    cursor: default;
+    transform: scale(1);
+}
+
+
+
+.showcase {
+    margin: auto;
+    max-width: 680px; 
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: rgb(255, 255, 255);
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+}
+
+.showcase li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 10px;
+}
+
+.showcase li small {
+    margin-left: 2px;
+}
+
+.container {
+    color: #777;
+    margin-top: 60px;
+    perspective: 1000px;
+    margin-bottom: 30px;
+}
+
+.screen {
+    color: #777;
+    margin-top: 60px;
+    perspective: 1000px;
+    margin-bottom: 30px;
+}
+
+.row {
+    margin: auto;
+    max-width: 680px; 
+    display: flex;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,38 +2,155 @@
 
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+
 import ChooseTicket from './components/ChooseTicket.vue'
-import Movies from './components/Movies.vue'
+import Home from './components/HomePage.vue'
 import MovieForm from './components/MovieForm.vue'
+import MovieUpdate from './components/MovieUpdate.vue'
+import AuditoriumUpdate from './components/AuditoriumUpdate.vue'
+import AssignmentUpdate from './components/AssignmentUpdate.vue'
+import AdminPanel from './components/AdminPanel.vue'
 
 
-const NotFound = { template: '<p>Page not found</p>' }
+// import { domain, clientId, audience } from '../auth_config.json'
+// import { Auth0Plugin } from '@/auth/auth0-plugin'
+
+
+import { domain, clientId, audience } from '../auth_config.json'
+import { Auth0Plugin } from '@/auth/auth0-plugin'
+import { authenticationGuard } from '@/auth/authenticationGuard'
 
 
 
+// new Vue({
+//   router,
+//   render: (h) => h(App),
+// }).$mount('#app');
+
+
+// const NotFound = { template: '<p>Page not found</p>' }
 
 
 
+Vue.use(Router);
 
-const routes = {
-  '/': App,
-  '/choose-ticket': ChooseTicket,
-  '/movies': Movies,
-  '/add-movie': MovieForm
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/choose-ticket/:id',
+      name: 'Choose Ticket',
+      component: ChooseTicket,
+      params: castPropToNumber,
+    },
+    {
+      path: '/add-movie',
+      name: 'Add Movie',
+      component: MovieForm,
+    },
+    {
+      path: '/update-movie/:id',
+      name: 'Update Movie',
+      component: MovieUpdate,
+      params: castPropToNumber,
+      beforeEnter: authenticationGuard,
+    },
+    {
+      path: '/update-assignment/:id',
+      name: 'Update Assignment',
+      component: AssignmentUpdate,
+      params: castPropToNumber,
+      beforeEnter: authenticationGuard,
+    },
+    {
+      path: '/update-auditorium/:id',
+      name: 'Update Auditorium',
+      component: AuditoriumUpdate,
+      params: castPropToNumber,
+      beforeEnter: authenticationGuard,
+    },
+    {
+      path: '/admin-panel',
+      name: 'Admin Panel',
+      component: AdminPanel,
+      beforeEnter: authenticationGuard,
+    },
+   
+  ]
+})
+
+function castPropToNumber(route) {
+  return {
+      id: Number(route.params.id)
+  };
 }
 
-new Vue({
-  el: '#app',
-  data () {
-    return {
-      currentRoute: window.location.pathname
-    }
+
+
+
+// const routes = {
+//   '/': App,
+//   '/choose-ticket': ChooseTicket,
+//   '/add-movie': MovieForm,
+//   '/admin-panel': AdminPanel
+// }
+
+// new Vue({
+//   el: '#app',
+//   data () {
+//     return {
+//       currentRoute: window.location.pathname
+//     }
     
+//   },
+//   computed: {
+//     ViewComponent () {
+//       return routes[this.currentRoute] || NotFound
+//     }
+//   },
+//   render (h) { return h(this.ViewComponent) }
+// })
+
+// Install the authentication plugin
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
   },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute] || NotFound
-    }
-  },
-  render (h) { return h(this.ViewComponent) }
-})
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
+
+// // Vue.use(Auth0Plugin, {
+// //   domain,
+// //   clientId,
+// //   audience,
+// //   onRedirectCallback: (appState) => {
+// //     routes.push(
+// //       appState && appState.targetUrl
+// //         ? appState.targetUrl
+// //         : window.location.pathname,
+// //     );
+// //   },
+// // });
+
+// // Vue.config.productionTip = false
+
